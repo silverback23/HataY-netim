32103e25df810ee06121a8daa1dda67824735188
silverback23 <cahid23@hotmail.com>
2021-10-31 11:22:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HataYönetim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
